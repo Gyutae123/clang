5b443a11337fd89baf92879e28fcb2322418491a
Gyutae123 <kkssaln@naver.com>
2026-05-26 16:29:53 +0900

    Create 3.c

---
+++ 26.05.26/3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+//int a = 10;
+//int main() {
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main() {
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int a = 10;
+
+//int a = 10;
+//int main() {
+//	extern int a;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main() {
+//	extern int a;
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int a = 10;
+
+//int a;
+//int main() {
+//	a = 10;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+int a = 10;
+int main() {
+	a = 6;
+	printf("%d\n", a);
+	return 0;
+}
