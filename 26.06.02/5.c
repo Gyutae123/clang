effcd5553709e784d531b6335f31ecac1aaeb940
Gyutae123 <kkssaln@naver.com>
2026-06-02 16:38:44 +0900

    Create 5.c

---
+++ 26.06.02/5.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+	int arr[3][3];
+	int count = 1;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			arr[i][j] = count;
+			count++;
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	
+	return 0;
+}
